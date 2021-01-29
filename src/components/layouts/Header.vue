cddc82bbf8c6e2e0653de89b26c9a9ef81b2d5a2
Thomas Gorszczyk <thomasgo77@gmail.com>
2021-01-29 20:22:40 +0100

    init vue ts project

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <nav class="fixed w-full px-2 bg-purple-900 opacity-75">
+      <div class="flex text-white text-lg font-bold py-4 justify-between">
+        <div class="flex">
+          <span class="px-2">Home</span>
+          <ul>
+            <li class="px-2">E-Palax</li>
+          </ul>
+        </div>
+        <div class="flex">
+          <button class="px-2">Login</button>
+          <button class="px-2">Sign Up</button>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header'
+});
+</script>
+
+<style>
+</style>
