94c88f0ba3f9bac7d59d5a7a1aa0643dc19bcbff
Tom Délié <tomdelie78@gmail.com>
2021-04-09 20:16:43 +0200

    style: add burger menu

--- src/components/layouts/default/Header.vue
+++ src/components/layouts/default/Header.vue
@@ -1,35 +1,37 @@
 <template>
   <header>
     <nav class="w-full bg-purple-1200 h-14 border-b z-50">
-      <div class="flex text-white text-sm mx-4 h-full items-center justify-between">
+      <div class="flex text-white text-sm mx-6 h-full items-center justify-between">
+        <HeaderBurger ref="burgerMenu" class="burger-menu hidden" />
         <div class="flex h-full items-center">
-          <a class="cursor-pointer text-lg">
-            <router-link to="/" class="flex items-center"><img src="/images/e-furrax.svg" width="28" /><span class="ml-3">E-FURRAX</span></router-link>
-          </a>
-          <ul class="h-full ml-14 flex items-center">
+          <img src="/images/icons/menu.svg" class="block md:hidden mr-6 cursor-pointer z-50" @click="handleBurger" />
+          <router-link to="/" class="text-lg flex items-center">
+            <img src="/images/e-furrax.svg" width="24" /><span class="ml-3 text-base">E-FURRAX</span>
+          </router-link>
+          <ul class="hidden md:flex h-full ml-8 items-center">
             <li
-              class="h-full mr-10 flex items-center hover:text-purple-500 cursor-pointer border-b-2 border-transparent hover:border-purple-500 transition"
+              class="h-full mr-8 flex items-center hover:text-purple-500 cursor-pointer border-b-2 border-transparent hover:border-purple-500 transition"
             >
               <router-link to="/search">Find your teammate</router-link>
             </li>
             <li
-              class="h-full mr-10 flex items-center hover:text-purple-500 cursor-pointer border-b-2 border-transparent hover:border-purple-500 transition"
+              class="h-full flex items-center hover:text-purple-500 cursor-pointer border-b-2 border-transparent hover:border-purple-500 transition"
             >
               <router-link to="/profile">Profile</router-link>
             </li>
           </ul>
         </div>
         <div class="flex items-center">
-          <SearchBar />
+          <SearchBar class="mr-4" />
           <router-link
             to="/sign-up"
-            class="px-3 py-1 mx-2 bg-purple-700 rounded hover:bg-purple-600 transition duration-300"
+            class="hidden md:block px-3 py-1 mx-2 bg-purple-700 rounded hover:bg-purple-600 transition duration-300"
           >
             Sign Up
           </router-link>
           <router-link
             to="/sign-in"
-            class="px-3 py-1 mx-2 bg-gray-700 hover:bg-gray-600 transition duration-300 rounded"
+            class="hidden md:block px-3 py-1 mx-2 bg-gray-700 hover:bg-gray-600 transition duration-300 rounded"
             >Log In</router-link
           >
         </div>
@@ -41,10 +43,24 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import SearchBar from '../../SearchBar.vue';
+import HeaderBurger from './HeaderBurger.vue';
 
 export default defineComponent({
-  components: { SearchBar },
-  name: 'Header'
+  components: { SearchBar, HeaderBurger },
+  name: 'Header',
+  provide() {
+    return {
+      handleBurger: this.handleBurger,
+    }
+  },
+  methods: {
+    handleBurger(event: { target: HTMLDivElement }) {
+      const burgerMenu = document.querySelector('.burger-menu') as HTMLDivElement;
+      const burgerNav = burgerMenu.querySelector('.burger-nav') as HTMLDivElement;
+      burgerMenu.classList.toggle('hidden');
+      burgerNav.classList.toggle('translate-x-240');
+    }
+  }
 });
 </script>
 
---
+++ src/components/layouts/default/HeaderBurger.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="h-full w-full fixed top-0 left-0 z-50">
+    <div class="burger-layer w-full h-full bg-black bg-opacity-50 transition duration-300" @click="handleBurger"></div>
+    <div
+      class="burger-nav absolute top-0 left-0 bg-purple-1200 h-full translate-x-240"
+      style="width: 240px"
+    >
+      <ul class="h-14 border-b border-purple-1000 flex items-center">
+        <img
+          src="/images/icons/menu.svg"
+          class="block pl-6 mr-6 cursor-pointer opacity-50"
+          @click="handleBurger"
+        />
+        <router-link to="/" class="text-lg flex items-center">
+          <img src="/images/e-furrax.svg" width="24" /><span class="ml-3 text-base">E-FURRAX</span>
+        </router-link>
+      </ul>
+      <ul class="py-3 border-b border-purple-1000">
+        <li class="cursor-pointer px-6 py-2 hover:bg-purple-1000">
+          <router-link to="/" class="flex items-center">
+            <img src="/images/icons/home.svg" class="opacity-50 mr-6" />
+            <span>Home</span>
+          </router-link>
+        </li>
+        <li class="cursor-pointer px-6 py-2 hover:bg-purple-1000">
+          <router-link to="/search" class="flex items-center">
+            <img src="/images/icons/explore.svg" class="opacity-50 mr-6" />
+            <span>Find your teammate</span>
+          </router-link>
+        </li>
+        <li class="cursor-pointer px-6 py-2 hover:bg-purple-1000">
+          <router-link to="/profile" class="flex items-center">
+            <img src="/images/icons/person.svg" class="opacity-50 mr-6" />
+            <span>Profile</span>
+          </router-link>
+        </li>
+      </ul>
+      <ul class="py-3 px-4 flex flex-col items-start">
+        <li
+          class="px-3 py-1 cursor-pointer bg-gray-700 hover:bg-gray-600 transition duration-300 rounded"
+        >
+          <router-link to="/sign-in"> Log In </router-link>
+        </li>
+        <li
+          class="px-3 py-1 mt-3 cursor-pointer bg-purple-700 hover:bg-purple-600 transition duration-300 rounded"
+        >
+          <router-link to="/sign-up"> Sign Up </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'HeaderBurger',
+  inject: ['handleBurger']
+});
+</script>
+
+<style lang="scss" scoped>
+.translate-x-240 {
+  transform: translateX(-240px);
+}
+
+.burger-nav {
+    transition: transform .3s ease;
+}
+</style>
+
