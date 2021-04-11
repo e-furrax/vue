eb046ff71f8409bd43b5f68b4ed7525f4dd3ede4
Tom Délié <43542343+tomdelie@users.noreply.github.com>
2021-04-11 16:35:08 +0200

    style: home page design and responsive (#26)
    * style: add efurrax logo
    * style: add home page background
    * style: add homepage responsive
    * style: add burger menu
    * style: add WaveSeparator and FeatureCard components
    * style: add transition on burger menu

---
+++ src/components/FeatureCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="text-center text-white flex flex-col items-center">
+    <div ref="floatingImg" class="floating"><slot name="icon" /></div>
+    <h3 class="text-2xl my-4"><slot name="title" /></h3>
+    <p><slot name="description" /></p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FeatureCard',
+  props: {},
+  mounted() {
+    const rand = 7 + Math.random() * 8;
+    const floatingImg = this.$refs.floatingImg as HTMLDivElement | null;
+    if (floatingImg) {
+      floatingImg.style.animationDuration = `${rand}s`;
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.floating {
+  filter: drop-shadow(4px 4px 4px rgba(0, 0, 0, 0.6));
+  animation-name: floating;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+}
+
+@keyframes floating {
+  0% {
+    transform: translate(0, 0px);
+  }
+  50% {
+    transform: translate(0, -15px);
+  }
+  100% {
+    transform: translate(0, -0px);
+  }
+}
+</style>
