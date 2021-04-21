1462c0b6c3d80e2dbc580a3841ccb92bb4db810f
Tom Délié <tomdelie78@gmail.com>
2021-04-21 20:12:22 +0200

    wip

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -9,8 +9,8 @@
       `"
       class="profile-header absolute top-0 left-0 w-full bg-no-repeat bg-cover bg-top opacity-40"
     ></div>
-    <div class="mt-60">
-      <div class="container mx-auto w-full flex justify-end">
+    <div class="mt-8 lg:mt-40">
+      <div class="container mx-auto w-full flex justify-center lg:justify-end">
         <button
           class="mr-4 uppercase rounded bg-transparent border border-purple-400 text-purple-400 text-sm py-2 px-6 hover:border-purple-300 hover:text-purple-300 transition-all ease-in duration-200"
         >
@@ -22,22 +22,24 @@
           Play together
         </button>
       </div>
-      <div class="container mx-auto mt-4 flex items-start">
-        <div class="mb-10 mr-4">
-          <div class="bg-purple-925 w-full rounded-sm border border-purple-custom relative mb-4 pb-3">
-            <div class="profile-card absolute transform -translate-y-1/2 flex items-center px-4">
+      <div class="container mx-auto mt-4 flex flex-col lg:flex-row items-start">
+        <div class="lg:mb-10 mb-4 lg:mr-4">
+          <div
+            class="bg-purple-925 bg-opacity-70 w-full sm:rounded-sm border border-purple-custom relative mb-4 pb-3"
+          >
+            <div
+              class="profile-card mt-4 lg:mt-0 lg:absolute lg:transform lg:-translate-y-1/2 flex items-center px-4"
+            >
               <img
                 src="/images/avatar1.png"
-                class="rounded-full border-2 border-yellow-800 z-20"
-                width="175"
-                height="175"
+                class="rounded-full border-2 border-yellow-800 z-20 w-32 lg:w-40"
               />
               <div
-                class="flex flex-col bg-purple-1000 pl-16 pr-16 py-2 z-10 border border-purple-custom transform -translate-x-12 rounded-sm"
+                class="flex flex-col lg:bg-purple-1000 ml-4 lg:pl-16 lg:pr-16 w-full lg:w-auto lg:py-2 lg:z-10 lg:border border-purple-custom lg:transform lg:-translate-x-12 rounded-sm"
               >
                 <div class="flex items-center">
-                  <h2 class="text-2xl mr-2">{{ user.username }}</h2>
-                  <img src="/images/icons/verified.svg" title="Verified user" />
+                  <h2 class="text-xl lg:text-2xl mr-2">{{ user.username }}</h2>
+                  <img src="/images/icons/verified.svg" title="Verified user" width="18" />
                 </div>
                 <div class="flex flex-col mt-2 text-sm">
                   <span>French, English</span>
@@ -46,17 +48,17 @@
                 </div>
               </div>
             </div>
-            <div class="flex items-center absolute top-3 right-4">
+            <div class="flex items-center absolute top-8 lg:top-4 right-4">
               <img
                 v-for="index of [1, 2, 3, 4]"
                 :key="index"
                 src="/images/icons/star.svg"
-                width="22"
+                width="18"
               />
-              <img src="/images/icons/star_half.svg" width="22" />
-              <span class="ml-2">(12)</span>
+              <img src="/images/icons/star_half.svg" width="18" />
+              <span class="ml-2 text-sm">(12)</span>
             </div>
-            <section style="margin-top: 87.5px" class="pt-4 px-4">
+            <section class="lg:mt-20 pt-4 px-4">
               <h4 class="font-bold uppercase">Games</h4>
               <div class="grid gap-2 xl:grid-cols-3 lg:grid-cols-2 grid-cols-1">
                 <GameRank background="/images/backgrounds/lowres/rocket-league.jpg">
@@ -93,65 +95,71 @@
                 </GameRank>
               </div>
             </section>
-            <section class="mt-4 pt-4 px-4">
+            <section class="pt-4 px-4">
               <h4 class="font-bold uppercase">About me</h4>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officiis reprehenderit
                 blanditiis omnis voluptas, commodi quia nisi in impedit alias placeat non nobis quo
-                quas illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Corrupti cumque ad veritatis similique molestias doloribus adipisci ut nulla
-                porro quisquam dolorum voluptatibus, explicabo et est dicta id itaque quia saepe!
+                quas illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Corrupti cumque ad veritatis similique molestias doloribus
+                adipisci ut nulla porro quisquam dolorum voluptatibus, explicabo et est dicta id
+                itaque quia saepe!
               </p>
             </section>
             <section class="px-4 bg-purple-925">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officiis reprehenderit
                 blanditiis omnis voluptas, commodi quia nisi in impedit alias placeat non nobis quo
-                quas illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Corrupti cumque ad veritatis similique molestias doloribus adipisci ut nulla
-                porro quisquam dolorum voluptatibus, explicabo et est dicta id itaque quia saepe!
+                quas illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Corrupti cumque ad veritatis similique molestias doloribus
+                adipisci ut nulla porro quisquam dolorum voluptatibus, explicabo et est dicta id
+                itaque quia saepe!
               </p>
             </section>
           </div>
-          <div class="bg-purple-925 w-full rounded-sm border border-purple-custom">
-            <div class="flex items-center">
-              <div class="p-4 md:p-6 text-center lg:text-left">
-                <h4 class="font-bold uppercase">Comments</h4>
-                <Commentary v-for="com in commentaries" :key="com.id" :commentary="com"></Commentary>
-                <div class="flex items-center cursor-pointer w-full py-6">
-                  <hr class="w-full border-1 border-purple-800" />
-                  <p class="text-xs text-purple-300 uppercase flex-no-wrap w-1/3 px-2 text-center">
-                    Load more comments
-                  </p>
-                  <hr class="w-full border-1 border-purple-800" />
-                </div>
+          <div
+            class="bg-purple-925 sm:bg-opacity-70 w-full lg:rounded-sm border border-purple-custom"
+          >
+            <div class="p-4 text-left">
+              <h4 class="font-bold uppercase">Comments ({{ comments.length }})</h4>
+              <Comment v-for="com in comments" :key="com.id" :comment="com" class="mt-2"></Comment>
+              <div class="flex items-center cursor-pointer w-full pt-6">
+                <hr class="w-full border-1 border-purple-800" />
+                <p
+                  class="text-xs text-purple-300 hover:underline hover:text-purple-200 uppercase flex-no-wrap w-1/3 px-2 text-center"
+                >
+                  Load more comments
+                </p>
+                <hr class="w-full border-1 border-purple-800" />
               </div>
             </div>
           </div>
         </div>
-        <div class="bg-purple-925 rounded-sm border bg-purple-925 border-purple-custom max-w-sm">
+        <div
+          class="bg-purple-925 bg-opacity-70 sm:rounded-sm border bg-purple-925 border-purple-custom lg:max-w-sm mb-10 lg:mb-0"
+        >
           <section class="pt-4 p-4">
             <h4 class="font-bold uppercase">Today's Agenda</h4>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officiis reprehenderit
-              blanditiis omnis voluptas, commodi quia nisi in impedit alias placeat non nobis quo quas
-              illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Corrupti cumque ad veritatis similique molestias doloribus adipisci ut nulla porro
-              quisquam dolorum voluptatibus, explicabo et est dicta id itaque quia saepe!
+              blanditiis omnis voluptas, commodi quia nisi in impedit alias placeat non nobis quo
+              quas illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Corrupti cumque ad veritatis similique molestias doloribus adipisci ut nulla
+              porro quisquam dolorum voluptatibus, explicabo et est dicta id itaque quia saepe!
             </p>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officiis reprehenderit
-              blanditiis omnis voluptas, commodi quia nisi in impedit alias placeat non nobis quo quas
-              illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Corrupti cumque ad veritatis similique molestias doloribus adipisci ut nulla porro
-              quisquam dolorum voluptatibus, explicabo et est dicta id itaque quia saepe!
+              blanditiis omnis voluptas, commodi quia nisi in impedit alias placeat non nobis quo
+              quas illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Corrupti cumque ad veritatis similique molestias doloribus adipisci ut nulla
+              porro quisquam dolorum voluptatibus, explicabo et est dicta id itaque quia saepe!
             </p>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officiis reprehenderit
-              blanditiis omnis voluptas, commodi quia nisi in impedit alias placeat non nobis quo quas
-              illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Corrupti cumque ad veritatis similique molestias doloribus adipisci ut nulla porro
-              quisquam dolorum voluptatibus, explicabo et est dicta id itaque quia saepe!
+              blanditiis omnis voluptas, commodi quia nisi in impedit alias placeat non nobis quo
+              quas illo iste illum reiciendis id? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Corrupti cumque ad veritatis similique molestias doloribus adipisci ut nulla
+              porro quisquam dolorum voluptatibus, explicabo et est dicta id itaque quia saepe!
             </p>
           </section>
         </div>
@@ -165,7 +173,7 @@ import { defineComponent } from 'vue';
 import { useQuery, useResult } from '@vue/apollo-composable';
 import { getUser } from '@/apollo/user.gql';
 
-import Commentary from '@/components/Commentary';
+import Comment from '@/components/Comment';
 import Loader from '@/components/Loader';
 import GameRank from '@/components/GameRank';
 
@@ -186,11 +194,11 @@ export default defineComponent({
     };
   },
   name: 'UserProfile',
-  components: { Commentary, Loader, GameRank },
+  components: { Comment, Loader, GameRank },
   data() {
     return {
       loaded: false,
-      commentaries: [
+      comments: [
         {
           id: 1,
           author: 'Thomas',
