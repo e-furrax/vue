0b7b87823875055998f65f4aa12508e197b6d0b0
Tom Délié <tomdelie78@gmail.com>
2021-06-11 14:57:30 +0200

    feat: add user profile connections page

--- src/router/myProfile.ts
+++ src/router/myProfile.ts
@@ -4,6 +4,7 @@ import Settings from '@/views/user-profile/Settings.vue';
 import HistoryTransactions from '@/views/user-profile/HistoryTransactions.vue';
 import Account from '@/views/user-profile/Account.vue';
 import Security from '@/views/user-profile/Security.vue';
+import Connections from '@/views/user-profile/Connections.vue';
 
 import Header from '@/components/layouts/default/Header.vue';
 import Footer from '@/components/layouts/default/Footer.vue';
@@ -38,6 +39,15 @@ export const myProfileRoutes: RouteRecordRaw = {
           name: 'Security',
           component: Security,
           meta: { additionalInformations: 'Change Password', icon: 'policy.svg' }
+        },
+        {
+          path: 'connections',
+          name: 'Connections',
+          component: Connections,
+          meta: {
+            additionalInformations: 'Connect your accounts to unlock special integrations.',
+            icon: 'settings_input_component.svg'
+          }
         }
       ]
     },
