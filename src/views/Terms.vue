b98075641c5144aa11c17cd40236aef1e6f6465a
Tom Délié <tomdelie78@gmail.com>
2021-06-30 22:53:09 +0200

    feat: add Terms and Privacy pages

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -1,110 +1,81 @@
 <template>
-  <div>
-    <h1 class="text-xl">Privacy Policy</h1>
-    <h2 class="font-semibold border-b-2 border-purple-800 mb-6 mx-4 md:mx-6">
-      GDPR Privacy Policy for E-FURRAX
-    </h2>
+  <div class="my-20 container mx-auto text-white">
+    <h1 class="text-3xl text-center font-bold">E-FURRAX Terms of Service</h1>
+    <p class="text-center mb-10">Last updated: 30th of June, 2021</p>
+
+    <h2 class="text-2xl text-indigo-400 mb-3">Terms</h2>
     <p>
-      At E-FURRAX, accessible at e-furrax.com, one of our main priorities is the privacy of our
-      visitors. This Privacy Policy document contains types of information that is collected and
-      recorded by E-FURRAX and how we use it. If you have additional questions or require more
-      information about our Privacy Policy, do not hesitate to contact us through email at
-      contact.efurrax@gmail.com.
+      By accessing this Website, accessible from e-furrax.com, you are agreeing to be bound by these
+      Website Terms and Conditions of Use and agree that you are responsible for the agreement with
+      any applicable local laws. If you disagree with any of these terms, you are prohibited from
+      accessing this site. The materials contained in this website are protected by copyright and
+      trade mark law.
     </p>
 
-    <h2 class="font-semibold border-b-2 border-purple-800 mb-6 mx-4 md:mx-6">
-      General Data Protection Regulation (GDPR)
-    </h2>
+    <h2 class="mt-6 text-2xl text-indigo-400 mb-3">Use License</h2>
     <p>
-      We are a Data Controller of your information. E-FURRAX legal basis for collecting and using
-      the personal information described in this Privacy Policy depends on the Personal Information
-      we collect and the specific context in which we collect the information:
+      Permission is granted to temporarily download one copy of the materials on E-FURRAX for
+      personal, non-commercial transitory viewing only. This is the grant of a license, not a
+      transfer of title, and under this license you may not:
     </p>
-    <ul>
-      <li>E-FURRAX needs to perform a contract with you</li>
-      <li>You have given E-FURRAX permission to do so</li>
-      <li>Processing your personal information is in E-FURRAX legitimate interests</li>
-      <li>E-FURRAX needs to comply with the law</li>
+    <ul class="list-disc list-inside ml-6">
+      <li>modify or copy the materials</li>
+      <li>use the materials for any commercial purpose or for any public display</li>
+      <li>attempt to reverse engineer any software contained on E-FURRAX</li>
+      <li>remove any copyright or other proprietary notations from the materials</li>
+      <li>
+        transferring the materials to another person or "mirror" the materials on any other server
+      </li>
     </ul>
-    <p>
-      E-FURRAX will retain your personal information only for as long as is necessary for the
-      purposes set out in this Privacy Policy. We will retain and use your information to the extent
-      necessary to comply with our legal obligations, resolve disputes, and enforce our policies. If
-      you are a resident of the European Economic Area (EEA), you have certain data protection
-      rights. If you wish to be informed what Personal Information we hold about you and if you want
-      it to be removed from our systems, please contact us. In certain circumstances, you have the
-      following data protection rights:
-    </p>
-    <ul>
-      <li>The right to access, update or to delete the information we have on you.</li>
-      <li>The right of rectification.</li>
-      <li>The right to object.</li>
-      <li>The right of restriction.</li>
-      <li>The right to data portability</li>
-      <li>The right to withdraw consent</li>
-    </ul>
-
-    <h2 class="font-semibold border-b-2 border-purple-800 mb-6 mx-4 md:mx-6">Log Files</h2>
-    <p>
-      E-FURRAX follows a standard procedure of using log files. These files log visitors when they
-      visit websites. All hosting companies do this and a part of hosting services' analytics. The
-      information collected by log files include internet protocol (IP) addresses, browser type,
-      Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the
-      number of clicks. These are not linked to any information that is personally identifiable. The
-      purpose of the information is for analyzing trends, administering the site, tracking users'
-      movement on the website, and gathering demographic information.
+    <p class="mt-3">
+      This will let E-FURRAX to terminate upon violations of any of these restrictions. Upon
+      termination, your viewing right will also be terminated and you should destroy any downloaded
+      materials in your possession whether it is printed or electronic format.
     </p>
 
-    <h2 class="font-semibold border-b-2 border-purple-800 mb-6 mx-4 md:mx-6">
-      Cookies and Web Beacons
-    </h2>
-    <p>E-FURRAX does not use cookies and web beacons.</p>
-
-    <h2 class="font-semibold border-b-2 border-purple-800 mb-6 mx-4 md:mx-6">Privacy Policies</h2>
+    <h2 class="mt-6 text-2xl text-indigo-400 mb-3">Disclaimer</h2>
     <p>
-      You may consult this list to find the Privacy Policy for each of the advertising partners of
-      E-FURRAX. Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or
-      Web Beacons that are used in their respective advertisements and links that appear on
-      E-FURRAX, which are sent directly to users' browser. They automatically receive your IP
-      address when this occurs. These technologies are used to measure the effectiveness of their
-      advertising campaigns and/or to personalize the advertising content that you see on websites
-      that you visit. Note that E-FURRAX has no access to or control over these cookies that are
-      used by third-party advertisers.
+      All the materials on E-FURRAX are provided “as is”. E-FURRAX makes no warranties, may it be
+      expressed or implied, therefore negates all other warranties. Furthermore, E-FURRAX does not
+      make any representations concerning the accuracy or reliability of the use of the materials on
+      its website or otherwise relating to such materials or any sites linked to this website.
     </p>
 
-    <h2 class="font-semibold border-b-2 border-purple-800 mb-6 mx-4 md:mx-6">
-      Third Part Privacy Policies
-    </h2>
+    <h2 class="mt-6 text-2xl text-indigo-400 mb-3">Limitations</h2>
     <p>
-      E-FURRAX’s Privacy Policy does not apply to other advertisers or websites. Thus, we are
-      advising you to consult the respective Privacy Policies of these third-party ad servers for
-      more detailed information. It may include their practices and instructions about how to
-      opt-out of certain options. You may find a complete list of these Privacy Policies and their
-      links here: Privacy Policy Links. You can choose to disable cookies through your individual
-      browser options. To know more detailed information about cookie management with specific web
-      browsers, it can be found at the browsers' respective websites. What Are Cookies?
+      E-FURRAX or its suppliers will not be hold accountable for any damages that will arise with
+      the use or inability to use the materials on E-FURRAX’s website, even if E-FURRAX or an
+      authorize representative of this Website has been notified, orally or written, of the
+      possibility of such damage. Some jurisdiction does not allow limitations on implied warranties
+      or limitations of liability for incidental damages, these limitations may not apply to you.
     </p>
 
-    <h2 class="font-semibold border-b-2 border-purple-800 mb-6 mx-4 md:mx-6">
-      Children's Information
-    </h2>
+    <h2 class="mt-6 text-2xl text-indigo-400 mb-3">Revisions and Errata</h2>
     <p>
-      Another part of our priority is adding protection for children while using the internet. We
-      encourage parents and guardians to observe, participate in, and/or monitor and guide their
-      online activity. E-FURRAX does not knowingly collect any Personal Identifiable Information
-      from children under the age of 13. If you think that your child provided this kind of
-      information on our website, we strongly encourage you to contact us immediately and we will do
-      our best efforts to promptly remove such information from our records.
+      The materials appearing on E-FURRAX’s Website may include technical, typographical, or
+      photographic errors. E-FURRAX will not promise that any of the materials in this website are
+      accurate, complete, or current. E-FURRAX may change the materials contained on its website at
+      any time without notice. E-FURRAX does not make any commitment to update the materials.
     </p>
 
-    <h2 class="font-semibold border-b-2 border-purple-800 mb-6 mx-4 md:mx-6">
-      Online Privacy Policy Only
-    </h2>
+    <h2 class="mt-6 text-2xl text-indigo-400 mb-3">Links</h2>
     <p>
-      This privacy policy applies only to our online activities and is valid for visitors to our
-      website with regards to the information that they shared and/or collect in E-FURRAX. This
-      policy is not applicable to any information collected offline or via channels other than this
-      website.
+      E-FURRAX has not reviewed all of the sites linked to its website and is not responsible for
+      the contents of any such linked site. The presence of any link does not imply endorsement by
+      E-FURRAX of the site. The use of any linked website is at the user's own risk.
+    </p>
+
+    <h2 class="mt-6 text-2xl text-indigo-400 mb-3">Site Terms of Use Modifications</h2>
+    <p>
+      E-FURRAX may revise these Terms of Use for its website at any time without prior notice. By
+      using this website, you are agreeing to be bound by the current version of these Terms and
+      Conditions of Use.
+    </p>
+
+    <h2 class="mt-6 text-2xl text-indigo-400 mb-3">Governing Law</h2>
+    <p>
+      Any claim related to E-FURRAX’s website shall be governed by the laws of France without
+      regards to its conflict of law provisions.
     </p>
   </div>
 </template>
